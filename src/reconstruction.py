b512acf010360224c4cfccc23c72a07c8da97f08
luanfs <luanfonseca93@gmail.com>
2022-06-08 08:24:01 -0300

    update in boundary conditions

--- src/reconstruction.py
+++ src/reconstruction.py
@@ -18,43 +18,50 @@
 import numpy as np
 
 ####################################################################################
-# Given the average values of a scalar field Q (Q), this routine constructs 
+# Given the average values of a scalar field Q, this routine constructs 
 # a piecewise parabolic aproximation of Q using its average value.
 ####################################################################################
 def ppm_reconstruction(Q, N):
     # Compute the slopes dQ0 (centered finite difference)
-    # Formula 1.7 from Collela and Woodward 1983 and Figure 2 from Carpenter et al 1989.
-    # interior = np.linspace(2, N+1, N, dtype=np.int32)
+    # Formula 1.7 from Collela and Woodward 1984 and Figure 2 from Carpenter et al 1990.
     dQ0 = np.zeros(N+5)
     dQ0[1:N+4] = 0.5*(Q[2:N+5] - Q[0:N+3]) # Interior values are in 2:N+2
 
     # Compute the slopes dQ (1-sided finite difference)
-    # Formula 1.8 from Collela and Woodward 1983 and Figure 2 from Carpenter et al 1989.
+    # Formula 1.8 from Collela and Woodward 1984 and Figure 2 from Carpenter et al 1990.
     dQ1 = np.zeros(N+5) # Right 1-sided difference
     dQ2 = np.zeros(N+5) # Left  1-sided difference
 
     # Right 1-sided finite difference
-    dQ1[0:N+4] = Q[1:N+5] - Q[0:N+4]
-    dQ1 = 2.0*dQ1
+    dQ1[1:N+4] = Q[2:N+5] - Q[1:N+4]
+    dQ1 = dQ1*0.5
 
     # Left  1-sided finite difference
-    dQ2[1:N+5] = Q[1:N+5] - Q[0:N+4]
-    dQ2 = 2.0*dQ2
+    dQ2[1:N+4] = Q[1:N+4] - Q[0:N+3]
+    dQ2 = dQ2*0.5
 
-    # Final slope - Formula 1.8 from Collela and Woodward 1983
-    dQ = np.minimum(abs(dQ0), abs(dQ1))
-    dQ = np.minimum(dQ, abs(dQ2))*np.sign(dQ0)
+    #print(dQ0[1:N+4])
+    #print(dQ1[1:N+4])
+    #print(dQ2[1:N+4])
+    #exit()
+
+    # Final slope - Formula 1.8 from Collela and Woodward 1984
+    dQ = np.zeros(N+5)
+    dQ[1:N+4] = np.minimum(abs(dQ0[1:N+4]), abs(dQ1[1:N+4]))
+    dQ[1:N+4] = np.minimum(dQ[1:N+4], abs(dQ2[1:N+4]))*np.sign(dQ0[1:N+4])
+    mask = ( (Q[2:N+5] - Q[1:N+4]) * (Q[1:N+4] - Q[0:N+3]) > 0.0 ) # Indexes where (Q_{j+1}-Q_{j})*(Q_{j}-Q{j-1}) > 0
+    dQ[1:N+4][~mask] = 0.0
     dQ = dQ0
 
-    # Values of Q at right edges (Q_(j+1/2)) - Formula 1.6 from Collela and Woodward 1983
-    Q_edges = np.zeros(N+5)
-    Q_edges[0:N+4] = 0.5*(Q[0:N+4] + Q[1:N+5]) - (dQ[1:N+5] - dQ[0:N+4])/6.0
+    # Values of Q at right edges (q_(j+1/2)) - Formula 1.6 from Collela and Woodward 1984
+    Q_edges = np.zeros(N+1)
+    Q_edges[0:N+1] = 0.5*(Q[1:N+2] + Q[2:N+3]) - (dQ[2:N+3] - dQ[1:N+2])/6.0
 
     # Assign values of Q_R and Q_L
     q_R = np.zeros(N+5)
-    q_R[2:N+2] = Q_edges[2:N+2]
+    q_R[2:N+2] = Q_edges[1:N+1]
     q_L = np.zeros(N+5)
-    q_L[2:N+2] = Q_edges[1:N+1]
+    q_L[2:N+2] = Q_edges[0:N]
 
     # Compute the polynomial coefs
     # q(x) = q_L + z*(dq + q6*(1-z)) z in [0,1]
