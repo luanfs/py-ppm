3ec96f330767c42de236de7ae05bdbfad451c785
luanfs <luanfonseca93@gmail.com>
2022-06-11 10:26:42 -0300

    sw and 2d dimensions

--- main.py
+++ main.py
@@ -11,31 +11,52 @@ import sys
 import os.path
 sys.path.append(srcdir)
 
-#Imports
-import configuration
+# Imports
+import configuration as conf
 from miscellaneous        import createDirs
-from parameters           import simulation_par
+from parameters           import simulation_par_1d
 from advection            import adv_1d
 from error_adv            import error_analysis_adv1d
-from error_reconstruction import error_analysis_recon
+from error_reconstruction import error_analysis_recon_1d
 
 # Create directories
 createDirs()
 
-# Get parameters
-N, dt, Tf, tc, ic, mono = configuration.get_parameters()
-simulation = simulation_par(N, dt, Tf, ic, tc, mono)
+dim, equation = conf.get_parameters()
 
-# Select test case
-if tc == 1:
-    # Advection routine
-    adv_1d(simulation, True)
-elif tc == 2:
-    # Advection error analysis
-    error_analysis_adv1d(simulation)
-elif tc == 3:
-    # Advection error analysis
-    error_analysis_recon(simulation)
+# One-dimensional tests
+if dim == 1:
+    # 1D advection test cases - parameters from par/adv1d.par
+    if equation == 'adv':
+        # Get parameters
+        N, dt, Tf, tc, ic, mono = conf.get_test_parameters_1d('adv_1d.par')
+        simulation = simulation_par_1d(N, dt, Tf, ic, tc, mono)
+
+        # Select test case
+        if tc == 1:
+            # Advection routine
+            adv_1d(simulation, True)
+        elif tc == 2:
+            # Advection error analysis
+            error_analysis_adv1d(simulation)
+        elif tc == 3:
+            # Reconstruction error analysis
+            error_analysis_recon_1d(simulation)
+        else:
+            print('Invalid test case.')
+            exit()
+
+    elif equation == 'sw':
+        print('Shallow-water is not implemented yet :(')
+
+    else:
+        print('Invalid equation!\n')
+        exit()
+
+# Two-dimensional tests
+elif dim == 2:
+    print('2d tests are not implemented yet :(')
+
 else:
-    print('Invalid test case.')
+    print('Invalid dimension!\n')
     exit()
