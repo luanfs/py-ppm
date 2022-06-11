b7281b5d6a17a321e837b210d34fa9b673aefa6e
luanfs <luanfonseca93@gmail.com>
2022-06-11 10:27:46 -0300

    get_test_parameters_1d

--- src/configuration.py
+++ src/configuration.py
@@ -7,12 +7,47 @@
 # (luan.santos@usp.br)
 ####################################################################################
 
-import math
 import os.path
-from parameters import pardir, graphdir
+from parameters import pardir
+
 def get_parameters():
     # The standard file configuration.par must exist in par/ directory 
     file_path = pardir+"configuration.par"
+
+    if os.path.exists(file_path): # The file exists
+        # Open the grid file
+        confpar = open(file_path, "r")
+        
+        # Read the grid file lines
+        confpar.readline()
+        confpar.readline()
+        dim = confpar.readline()
+        confpar.readline()
+        equation = confpar.readline()
+        confpar.readline()
+
+        # Convert from str to int
+        dim  = int(dim)
+        equation  = str. rstrip(equation)
+        # Close the file
+        confpar.close()
+
+        #Print the parameters on the screen
+        print("\n--------------------------------------------------------")
+        print("Parameters from file", file_path,"\n")
+        print("Dimension: ", dim)
+        print("Equation: ", equation)
+        print("--------------------------------------------------------\n")
+    
+    else:   # The file does not exist
+        print("ERROR in get_grid_parameters: file configuration.par not found in /par.")
+        exit()
+    return dim, equation
+
+
+def get_test_parameters_1d(filename):
+    # The standard file filename.par must exist in par/ directory 
+    file_path = pardir+filename
 
     if os.path.exists(file_path): # The file exists
         # Open the grid file
@@ -50,13 +85,9 @@ def get_parameters():
         print("Parameters from file", file_path,"\n")
         print("Number of cells: ", N)
         print("Time step ", dt)
-        print("Total period definition ", Tf)
-        print("Initial condition: ", ic)
-        print("Test case: ", tc)
-        print("Monotonization: ", mono)
         print("--------------------------------------------------------\n")
     
     else:   # The file does not exist
-        print("ERROR in get_grid_parameters: file configuration.par not found in /par.")
+        print("ERROR in get_grid_parameters: file "+ filename +" not found in /par.")
         exit()
     return N, dt, Tf, tc, ic, mono
