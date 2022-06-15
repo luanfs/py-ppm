6914eb2e0b962408f66dcc0b82e3bf8f51490b46
luanfs <luanfonseca93@gmail.com>
2022-06-15 12:53:24 -0300

    2d routines

--- src/configuration.py
+++ src/configuration.py
@@ -8,7 +8,7 @@
 ####################################################################################
 
 import os.path
-from parameters import pardir
+from parameters_1d import pardir
 
 def get_parameters():
     # The standard file configuration.par must exist in par/ directory 
@@ -29,6 +29,7 @@ def get_parameters():
         # Convert from str to int
         dim  = int(dim)
         equation  = str. rstrip(equation)
+
         # Close the file
         confpar.close()
 
@@ -91,3 +92,54 @@ def get_test_parameters_1d(filename):
         print("ERROR in get_grid_parameters: file "+ filename +" not found in /par.")
         exit()
     return N, dt, Tf, tc, ic, mono
+
+def get_test_parameters_2d(filename):
+    # The standard file filename.par must exist in par/ directory 
+    file_path = pardir+filename
+
+    if os.path.exists(file_path): # The file exists
+        # Open the grid file
+        confpar = open(file_path, "r")
+        
+        # Read the grid file lines
+        confpar.readline()
+        confpar.readline()
+        N = confpar.readline()
+        confpar.readline()
+        M = confpar.readline()
+        confpar.readline()
+        Tf = confpar.readline()
+        confpar.readline()
+        dt = confpar.readline()
+        confpar.readline()
+        ic = confpar.readline()
+        confpar.readline()
+        tc = confpar.readline()
+        confpar.readline()
+        mono = confpar.readline()
+        confpar.readline()
+ 
+        # Close the file
+        confpar.close()
+
+        # Convert from str to int
+        N  = int(N)
+        M  = int(M)
+        Tf = float(Tf)
+        dt = float(dt)
+        ic = int(ic)
+        tc = int(tc)
+        mono = int(mono)
+
+        #Print the parameters on the screen
+        print("\n--------------------------------------------------------")
+        print("Parameters from file", file_path,"\n")
+        print("Number of cells in x direction: ", N)
+        print("Number of cells in y direction: ", M)
+        print("Time step ", dt)
+        print("--------------------------------------------------------\n")
+    
+    else:   # The file does not exist
+        print("ERROR in get_grid_parameters: file "+ filename +" not found in /par.")
+        exit()
+    return N, M, dt, Tf, tc, ic, mono
