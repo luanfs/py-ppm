a57493645212acfd5b797821806a6b1f71d5f483
luanfs <luanfonseca93@gmail.com>
2022-06-15 15:46:07 -0300

    2d adv

--- main.py
+++ main.py
@@ -13,11 +13,16 @@ sys.path.append(srcdir)
 
 # Imports
 import configuration as conf
-from miscellaneous        import createDirs
-from parameters           import simulation_par_1d
-from advection            import adv_1d
-from error_adv            import error_analysis_adv1d
-from error_reconstruction import error_analysis_recon_1d
+from miscellaneous           import createDirs
+
+from parameters_1d           import simulation_par_1d
+from advection_1d            import adv_1d
+from error_adv_1d            import error_analysis_adv1d
+from error_reconstruction_1d import error_analysis_recon_1d
+
+from advection_2d            import adv_2d
+from parameters_2d           import simulation_par_2d
+from error_adv_2d            import error_analysis_adv2d
 
 # Create directories
 createDirs()
@@ -55,8 +60,22 @@ if dim == 1:
 
 # Two-dimensional tests
 elif dim == 2:
-    print('2d tests are not implemented yet :(')
+    # 2D advection test cases - parameters from par/adv1d.par
+    if equation == 'adv':
+        # Get parameters
+        N, M, dt, Tf, tc, ic, mono = conf.get_test_parameters_2d('adv_2d.par')
+        simulation = simulation_par_2d(N, M, dt, Tf, ic, tc, mono)
 
+        # Select test case
+        if tc == 1:
+            # Advection routine
+            adv_2d(simulation, True)
+        else:
+            # Advection error analysis
+            error_analysis_adv2d(simulation)
+
+    elif equation == 'sw':
+        print('Shallow-water is not implemented yet :(')
 else:
     print('Invalid dimension!\n')
     exit()
