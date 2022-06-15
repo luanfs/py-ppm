c23745c1d3f0869d7a4963cfeb3c0c7ff43a0bca
luanfs <luanfonseca93@gmail.com>
2022-06-15 12:43:40 -0300

    minor changes

--- src/advection_1d.py
+++ src/advection_1d.py
@@ -4,15 +4,15 @@
 # Luan da Fonseca Santos - April 2022
 # Solves the PDE Q_t+ u*Q_x = 0 with periodic boundary conditions
 # The initial condition Q(x,0) is given in the module parameters.py
-# 
+#
 # References:
 # -  Phillip Colella, Paul R Woodward, The Piecewise Parabolic Method (PPM) for gas-dynamical simulations,
 # Journal of Computational Physics, Volume 54, Issue 1, 1984, Pages 174-201, ISSN 0021-9991,
 # https://doi.org/10.1016/0021-9991(84)90143-8.
 #
-# -  Carpenter , R. L., Jr., Droegemeier, K. K., Woodward, P. R., & Hane, C. E. (1990). 
+# -  Carpenter , R. L., Jr., Droegemeier, K. K., Woodward, P. R., & Hane, C. E. (1990).
 # Application of the Piecewise Parabolic Method (PPM) to Meteorological Modeling, Monthly Weather Review, 118(3),
-# 586-612. Retrieved Mar 31, 2022, 
+# 586-612. Retrieved Mar 31, 2022,
 # from https://journals.ametsoc.org/view/journals/mwre/118/3/1520-0493_1990_118_0586_aotppm_2_0_co_2.xml
 #
 ####################################################################################
@@ -49,7 +49,7 @@ def adv_1d(simulation, plot):
 
     # Compute average values of Q (initial condition)
     Q = np.zeros(N+5)
-    if (simulation.ic == 0 or simulation.ic == 1 or simulation.ic == 3 or simulation.ic == 4):
+    if (simulation.ic == 0 or simulation.ic == 1 or simulation.ic == 3 or simulation.ic == 4 or simulation.ic == 5):
         Q[2:N+2] = (q0_antiderivative_adv(x[1:N+1], simulation) - q0_antiderivative_adv(x[0:N], simulation))/dx
     elif (simulation.ic == 2):
         #Q[2:N+2] = q0(xc,simulation)
@@ -68,7 +68,7 @@ def adv_1d(simulation, plot):
     ymax = np.amax(q_exact)
     dists = abs(np.add.outer(xplot,-xc))
     neighbours = dists.argmin(axis=1)
-  
+
     # Compute initial mass
     total_mass0, mass_change = diagnostics_adv_1d(Q, simulation, 1.0)
 
@@ -80,7 +80,7 @@ def adv_1d(simulation, plot):
     # Time looping
     for t in range(0, Nsteps+1):
         # Velocity update
-        u_edges[0:N+1] = velocity_adv_1d(x, 0, simulation)
+        u_edges[0:N+1] = velocity_adv_1d(x, t*dt, simulation)
 
         # Applies a PPM time step
         Q, dq, q6, q_L, _ = time_step_adv1d_ppm(Q, u_edges, N, simulation)
@@ -111,9 +111,8 @@ def adv_1d(simulation, plot):
             # Plot the graph and print diagnostic
             #title = simulation.title +'- '+icname+' - time='+str(t*dt)+', CFL='+str(CFL)+',\n N='+str(N)+', '+simulation.fvmethod+', mono = '+simulation.monot
             #filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_t'+str(t)+'_N'+str(N)+'_'+simulation.fvmethod+'_mono'+simulation.monot+'.png'
-            #plot_field_graphs([q_exact, q_parabolic], ['Exact', 'Parabolic'], xplot, ymin, ymax, filename, title)
+            #plot_1dfield_graphs([q_exact, q_parabolic], ['Exact', 'Parabolic'], xplot, ymin, ymax, filename, title)
             print_diagnostics_adv_1d(error_linf[t], error_l1[t], error_l2[t], mass_change, t, Nsteps)
-
     #---------------------------------------End of time loop---------------------------------------
 
     if plot:
@@ -121,7 +120,7 @@ def adv_1d(simulation, plot):
         title = simulation.title +'- '+icname+', CFL='+str(CFL)+',\n N='+str(N)+', '+simulation.fvmethod+', mono = '+simulation.monot
         filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_N'+str(N)+'_'+simulation.fvmethod+'_mono'+simulation.monot+'_erros.png'
         plot_time_evolution([error_linf, error_l1, error_l2], Tf, ['$L_\infty}$','$L_1$','$L_2$'], 'Error', filename, title)
-        
+
         # Plot the solution graph
         qmin = str("{:.2e}".format(np.amin(q_parabolic)))
         qmax = str("{:.2e}".format(np.amax(q_parabolic)))
@@ -140,15 +139,16 @@ def adv_1d(simulation, plot):
         # Compute exact solution
         q_exact = qexact_adv(xplot, Tf, simulation)
         q_exact_edges = qexact_adv(x, 0, simulation)
-        
+
         # Relative errors in different metrics
         error_inf, error_1, error_2 = compute_errors(q_parabolic, q_exact)
         error_ed_linf, error_ed_l1, error_ed_l2 = compute_errors(q_exact_edges[0:N], q_L[2:N+2])
-        
+
         # Plot the solution graph
         qmin = str("{:.2e}".format(np.amin(q_parabolic)))
         qmax = str("{:.2e}".format(np.amax(q_parabolic)))
         title = '1D advection - '+icname+' - time='+str(t*dt)+', CFL='+str(CFL)+',\n N='+str(N)+', '+simulation.fvmethod+', mono = '+simulation.monot+ ', Min = '+ qmin +', Max = '+qmax
         filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_t'+str(t)+'_N'+str(N)+'_'+simulation.fvmethod+'_mono'+simulation.monot+'.png'
         plot_1dfield_graphs([q_exact, q_parabolic], ['Exact', 'Parabolic'], xplot, ymin, ymax, filename, title)
+
         return error_inf, error_1, error_2, error_ed_linf, error_ed_l1, error_ed_l2
