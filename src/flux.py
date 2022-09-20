a9f057c310291bd8643ead057ae78753bf776977
luanfs <luanfonseca93@gmail.com>
2022-09-20 14:42:04 -0300

    ghost cells update

--- src/flux.py
+++ src/flux.py
@@ -16,35 +16,32 @@
 # (luan.santos@usp.br)
 ####################################################################################
 import numpy as np
-def numerical_flux(F, f_R, f_L, Q, q_R, q_L, dq, q6, u_edges, simulation, N):
+def numerical_flux(Q, q_R, q_L, dq, q6, u_edges, simulation, N):    
+    i0    = simulation.i0         # Interior of grid indexes
+    iend  = simulation.iend
+    ng    = simulation.ng         # Total number of ghost cells
+    ngr   = simulation.ng_right   # Number of ghost cells at right
+    ngl   = simulation.ng_left    # Number of ghost cells at left
+
+    # Numerical fluxes at edges
+    f_L = np.zeros(N+1) # Left
+    f_R = np.zeros(N+1) # Right
+
     # Compute the fluxes (formula 1.12 from Collela and Woodward 1984)
     c = u_edges*(simulation.dt/simulation.dx) #cfl number
     c2 = c*c
-    # Flux at left edges
-    f_L[1:N+1] = q_R[2:N+2] - c[0:N]*0.5*(dq[2:N+2] - (1.0-(2.0/3.0)*c[0:N])*q6[2:N+2])
-    #f_L[1:N+1] =  q_R[2:N+2]  + c[0:N]*0.5*(q6[2:N+2] -dq[2:N+2]) - q6[2:N+2]*c[0:N]**2/3.0
-    f_L[0] = f_L[N]
-    #f_L[1:N+1] =       (        1.0*c[0:N] -  1.0*c2[0:N])*Q[0:N]
-    #f_L[1:N+1] = f_L[1:N+1] + (-1.0 -  5.0*c[0:N] +  6.0*c2[0:N])*Q[1:N+1]
-    #f_L[1:N+1] = f_L[1:N+1] + ( 7.0 + 15.0*c[0:N] - 10.0*c2[0:N])*Q[2:N+2]
-    #f_L[1:N+1] = f_L[1:N+1] + ( 7.0 - 13.0*c[0:N] +  6.0*c2[0:N])*Q[3:N+3]
-    #f_L[1:N+1] = f_L[1:N+1] + (-1.0 +  2.0*c[0:N] -  1.0*c2[0:N])*Q[4:N+4]
-    #f_L[1:N+1] = f_L[1:N+1]/12.0
 
-    # Flux at right edges
+    # Flux from left at edges
+    f_L[0:N+1] = q_R[i0-1:iend] - c[i0-1:iend]*0.5*(dq[i0-1:iend] - (1.0-(2.0/3.0)*c[i0-1:iend])*q6[i0-1:iend])
+
+    # Flux from right at edges
     c = -c
-    f_R[1:N+1] = q_L[3:N+3] + c[1:N+1]*0.5*(dq[3:N+3] + (1.0-2.0/3.0*c[1:N+1])*q6[3:N+3])
-    #f_R[1:N+1]= q_L[3:N+3] + c[1:N+1]*0.5*(dq[3:N+3] + q6[3:N+3]) - q6[3:N+3]*c[1:N+1]**2/3.0
-    f_R[0] = f_R[N] # Periodic bc
+    f_R[0:N+1] = q_L[i0:iend+1] + c[i0:iend+1]*0.5*(dq[i0:iend+1] + (1.0-2.0/3.0*c[i0:iend+1])*q6[i0:iend+1])
 
-    #fR = (-1.0 + 2.0*c[1:N+1] -c[1:N+1]*c[1:N+1])*Q[1:N+1]
-    #fR = fR + (7.0 -13.0*c[1:N+1] +6.0*c[1:N+1]*c[1:N+1])*Q[2:N+2]
-    #fR = fR + (7.0 + 15.0*c[1:N+1] - 10.0*c[1:N+1]*c[1:N+1])*Q[3:N+3]
-    #fR = fR + (-1.0 - 5.0*c[1:N+1] + 6.0*c[1:N+1]*c[1:N+1])*Q[4:N+4]
-    #fR = fR + (1.0*c[1:N+1]-1.0*c[1:N+1]*c[1:N+1])*Q[5:N+5]
-    #fR = fR/12.0
-    #print(np.amax(abs(fR-f_R[1:N+1])))
-    #exit()
     # F - Formula 1.13 from Collela and Woodward 1984)
-    F[u_edges >= 0] = f_L[u_edges >= 0]
-    F[u_edges <= 0] = f_R[u_edges <= 0]
+    F = np.zeros(N+1) # Numerical flux
+    mask = u_edges[i0:iend+1] >=0
+
+    F[u_edges[i0:iend+1] >= 0] = f_L[u_edges[i0:iend+1] >= 0]
+    F[u_edges[i0:iend+1] <= 0] = f_R[u_edges[i0:iend+1] <= 0]
+    return F
