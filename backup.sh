5de39ce4ac1959de32c5934727c769bf6540a70a
luanfs <luanfonseca93@gmail.com>
2022-12-05 08:26:09 -0300

    bkp files

---
+++ backup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Backup script
+source sh/tarfiles.sh
+source sh/syncfiles.sh
---
+++ sh/tarfiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Creates a tarball
+
+date=` date +%F `
+version=` date +%y.%m.%d `
+echo "Today: " $date
+
+sourcefiles="src/*.py"
+
+parfiles="par/*.par "
+
+scripts="sh/*.sh "
+
+others="main.py\
+ README.*"
+
+files="$sourcefiles $parfiles $scripts $others"
+
+output="py-ppm$version.tar.bz2"
+
+tar cjfv $output $files
+
+echo "File " $output " ready!"
+echo
+
+echo "-------------------------------------------"
