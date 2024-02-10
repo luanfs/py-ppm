c5d57f91b798769aafb03a775b3021f094e8dbf3
luanfs <luanfonseca93@gmail.com>
2024-02-10 14:18:16 -0300

    updated parameters to be tested in convergence

--- src/advection_error.py
+++ src/advection_error.py
@@ -29,18 +29,18 @@ def error_analysis_adv1d(simulation):
     tc = simulation.tc
 
     # CFL number for all simulations
-    CFL = 0.8
+    CFL = 0.1
 
     # Interval
     x0 = simulation.x0
     xf = simulation.xf
 
     # Number of tests
-    Ntest = 7
+    Ntest = 4
 
     # Number of cells
     N = np.zeros(Ntest)
-    N[0] = 16
+    N[0] = 48
 
     # Array of time steps
     dt = np.zeros(Ntest)
@@ -51,76 +51,75 @@ def error_analysis_adv1d(simulation):
     elif simulation.vf==2: #variable velocity
         u0 = 0.4
         dt[0] = CFL/(N[0])*(xf-x0)/u0
+        dt[0] = 0.04
         Tf = 5.0
     else:
         print("ERROR: invalid vector field ", simulation.vf)
         exit()
 
     # Errors array
-    recons = (1,2,3,4)
-    deps = (1,2)
+    recons = (1,1,4,4)
+    deps = (1,2,1,2)
     #recons = (simulation.recon,)
     #deps = (simulation.dp,)
-    recon_names = ['PPM-0', 'PPM-CW84','PPM-PL07','PPM-L04']
+    #recon_names = ['PPM-0', 'PPM-CW84','PPM-PL07','PPM-L04']
     dp_names = ['RK1', 'RK2','RK3']
-    error_linf = np.zeros((Ntest, len(recons), len(deps)))
-    error_l1   = np.zeros((Ntest, len(recons), len(deps)))
-    error_l2   = np.zeros((Ntest, len(recons), len(deps)))
+    error_linf = np.zeros((Ntest, len(deps)))
+    error_l1   = np.zeros((Ntest, len(deps)))
+    error_l2   = np.zeros((Ntest, len(deps)))
 
     # Compute number of cells and time step for each simulation
     for i in range(1, Ntest):
         N[i]  = N[i-1]*2.0
         dt[i] = dt[i-1]*0.5
 
-    d = 0
-    for dp in deps:
-        rec = 0
-        for recon in recons:
-            # Let us test and compute the error
-            for i in range(0, Ntest):
-                # Update simulation parameters
-                simulation = simulation_adv_par_1d(int(N[i]), dt[i], Tf, ic, vf, tc, recon, dp)
+    for k in range(0,len(deps)):
+        dp = deps[k]
+        recon = recons[k]
+        # Let us test and compute the error
+        for i in range(0, Ntest):
+            # Update simulation parameters
+            simulation = simulation_adv_par_1d(int(N[i]), dt[i], Tf, ic, vf, tc, recon, dp)
 
-                # Run advection routine and get the errors
-                error_linf[i,rec,d], error_l1[i,rec,d], error_l2[i,rec,d] =  adv_1d(simulation, False)
-                print('\nParameters: N = '+str(int(N[i]))+', dt = '+str(dt[i]),', recon = ', recon, ', dp = ', dp)
+            # Run advection routine and get the errors
+            error_linf[i,k], error_l1[i,k], error_l2[i,k] =  adv_1d(simulation, False)
+            print('\nParameters: N = '+str(int(N[i]))+', dt = '+str(dt[i]),', recon = ', recon, ', dp = ', dp)
 
-                # Output
-                print_errors_simul(error_linf[:,rec,d], error_l1[:,rec,d], error_l2[:,rec,d], i)
-            rec = rec+1
-        d = d+1
+            # Output
+            print_errors_simul(error_linf[:,k], error_l1[:,k], error_l2[:,k], i)
 
     # plot errors for different all schemes in  different norms
     error_list = [error_linf, error_l1, error_l2]
     norm_list  = ['linf','l1','l2']
     norm_title  = [r'$L_{\infty}$',r'$L_1$',r'$L_2$']
 
-    for d in range(0, len(deps)):
-        e = 0
-        for error in error_list:
-            emin, emax = np.amin(error[:]), np.amax(error[:])
+    e = 0
+    for errors in error_list:
+        emin, emax = np.amin(errors), np.amax(errors)
+     
+        # convergence rate min/max
+        n = len(errors)
+        CR = np.abs(np.log(errors[1:n])-np.log(errors[0:n-1]))/np.log(2.0)
+        CRmin, CRmax = np.amin(CR), np.amax(CR)       
 
-            # convergence rate min/max
-            n = len(error)
-            CR = np.abs(np.log(error[1:n])-np.log(error[0:n-1]))/np.log(2.0)
-            CRmin, CRmax = np.amin(CR), np.amax(CR)
-            errors = []
-            dep_name = []
-            for r in range(0, len(recons)):
-                errors.append(error[:,r,d])
-                dep_name.append(recon_names[recons[r]-1])
+        title = simulation.title + ' - ' + simulation.icname+', vf='+ str(simulation.vf)+\
+            ', norm='+norm_title[e]
+        filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_vf'+str(vf)+\
+            '_norm'+norm_list[e]+'_parabola_errors.pdf'
 
-            title = simulation.title + ' - ' + simulation.icname+', vf='+ str(simulation.vf)+\
-            ', dp = '+dp_names[deps[d]-1]+', norm='+norm_title[e]
-            filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_vf'+str(vf)+'_dp'+dp_names[deps[d]-1]\
-            +'_norm'+norm_list[e]+'_parabola_errors.pdf'
+        names = []
+        errors_list = []
+        for k in range(0,len(deps)):
+            recon = recons[k]
+            dp = deps[k]
+            names.append('PPM'+str(recon)+' - RK'+str(dp)) 
+            errors_list.append(errors[:,k])
+        plot_errors_loglog(N, errors_list, names, filename, title, emin, emax)
 
-            plot_errors_loglog(N, errors, dep_name, filename, title, emin, emax)
-
-            # Plot the convergence rate
-            title = 'Convergence rate - ' + simulation.icname +', vf=' + str(simulation.vf)+\
-            ', dp = '+dp_names[deps[d]-1]+', norm='+norm_title[e]
-            filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_vf'+str(vf)+'_dp'+dp_names[deps[d]-1]\
-            +'_norm'+norm_list[e]+'_convergence_rate.pdf'
-            plot_convergence_rate(N, errors, dep_name, filename, title, CRmin, CRmax)
-            e = e+1
+        # Plot the convergence rate
+        title = 'Convergence rate - ' + simulation.icname +', vf=' + str(simulation.vf)+\
+        ', norm='+norm_title[e]
+        filename = graphdir+'1d_adv_tc'+str(tc)+'_ic'+str(ic)+'_vf'+str(vf)+\
+        '_norm'+norm_list[e]+'_convergence_rate.pdf'
+        plot_convergence_rate(N, errors, names, filename, title, CRmin, CRmax)
+        e = e+1
